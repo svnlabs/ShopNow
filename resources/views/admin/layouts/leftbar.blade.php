f09d63373c67b61b7214d5af230f75ffea55e67d
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-11 17:56:21 +0600

    Create leftbar.blade.php

---
+++ resources/views/admin/layouts/leftbar.blade.php
@@ -0,0 +1,65 @@
+<div id="sidebar-left" class="span2">
+	<div class="nav-collapse sidebar-nav">
+		<ul class="nav nav-tabs nav-stacked main-menu">
+			<li><a href="{{url('admin')}}"><i class="halflings-icon home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+			<li>
+				<a class="dropmenu" href="#"><i class=" halflings-icon plus-sign"></i><span class="hidden-tablet">Catagory</span></a>
+				<ul>
+					<li><a class="submenu" href="{{url('admin/category')}}"><i class=" halflings-icon plus-sign"></i><span class="hidden-tablet">All Catagory</span></a></li>
+					<li><a class="submenu" href="{{url('admin/category/create')}}"><i class="halflings-icon plus-sign"></i><span class="hidden-tablet">New Catagory</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="dropmenu" href="#"><i class="halflings-icon user"></i><span class="hidden-tablet">Customer</span></a>
+				<ul>
+					<li><a class="submenu" href="{{url('admin/customer')}}"><i class="halflings-icon user"></i><span class="hidden-tablet">All Customer</span></a></li>
+					<li><a class="submenu" href="{{url('admin/customer/create')}}"><i class="halflings-icon user"></i><span class="hidden-tablet">New Customer</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="dropmenu" href="#"><i class="halflings-icon barcode"></i><span class="hidden-tablet">Manufacturer</span></a>
+				<ul>
+					<li><a class="submenu" href="{{url('admin/manufacturer')}}"><i class="halflings-icon barcode"></i><span class="hidden-tablet">All Manufacturer</span></a></li>
+					<li><a class="submenu" href="{{url('admin/manufacturer/create')}}"><i class="halflings-icon barcode"></i><span class="hidden-tablet">New Manufacturer</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="dropmenu" href="#"><i class="halflings-icon briefcase"></i><span class="hidden-tablet">Order</span></a>
+				<ul>
+					<li><a class="submenu" href="{{url('admin/order')}}"><i class="halflings-icon briefcase"></i><span class="hidden-tablet">All Order</span></a></li>
+					<li><a class="submenu" href="{{url('admin/order/create')}}"><i class="halflings-icon briefcase"></i><span class="hidden-tablet">New Order</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="dropmenu" href="#"><i class="halflings-icon shopping-cart"></i><span class="hidden-tablet">Payment</span></a>
+				<ul>
+					<li><a class="submenu" href="{{url('admin/payment')}}"><i class="halflings-icon shopping-cart"></i><span class="hidden-tablet">All Payment</span></a></li>
+					<li><a class="submenu" href="{{url('admin/payment/create')}}"><i class="halflings-icon shopping-cart"></i><span class="hidden-tablet">New Payment</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="dropmenu" href="#"><i class="halflings-icon gift"></i><span class="hidden-tablet">Product</span></a>
+				<ul>
+					<li><a class="submenu" href="{{url('admin/product')}}"><i class="halflings-icon gift"></i><span class="hidden-tablet">All Product</span></a></li>
+					<li><a class="submenu" href="{{url('admin/product/create')}}"><i class="halflings-icon gift"></i><span class="hidden-tablet">New Product</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="dropmenu" href="#"><i class="halflings-icon shopping-cart"></i><span class="hidden-tablet">Shipping</span></a>
+				<ul>
+					<li><a class="submenu" href="{{url('admin/shipping')}}"><i class="halflings-icon shopping-cart"></i><span class="hidden-tablet">All Shipping</span></a></li>
+					<li><a class="submenu" href="{{url('admin/shipping/create')}}"><i class="halflings-icon shopping-cart"></i><span class="hidden-tablet">New Shipping</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a class="dropmenu" href="#"><i class=" halflings-icon user"></i><span class="hidden-tablet"> User</span></a>
+				<ul>
+					<li><a class="submenu" href="{{url('admin/user')}}"><i class=" halflings-icon user"></i><span class="hidden-tablet"> All User</span></a></li>
+					<li><a class="submenu" href="{{url('admin/user/create')}}"><i class="halflings-icon user"></i><span class="hidden-tablet">New User</span></a></li>
+				</ul>
+			</li>
+
+
+		</ul>
+	</div>
+</div>
