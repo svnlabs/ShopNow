54b23815b7aa8da0f94d3def7ce4260fb6092ca3
nasimasheikh <52200293+nasimasheikh@users.noreply.github.com>
2019-07-13 16:09:56 +0600

    Create 2019_07_13_100022_create_transactions_table.php

---
+++ database/migrations/2019_07_13_100022_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id');
+            $table->integer('transaction_id');
+            $table->string('payment_method',191);
+            $table->timestamp('delete_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
