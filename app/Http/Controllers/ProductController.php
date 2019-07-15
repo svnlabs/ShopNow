2b70c1c7629ffcea3877d7922f9bd8342cd58b7d
Kabir <khyrulkabir01@gmail.com>
2019-07-15 18:41:01 +0600

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
 use Illuminate\Support\Str;
+use Alert;
+use File;
 class ProductController extends Controller
 {
     /**
@@ -67,7 +69,7 @@ class ProductController extends Controller
         
         
         $product->save();
-        return redirect('/product')->with('success','Product Added');
+        return redirect('/product')->with('alert','Product Added');
     }
 
     /**
@@ -103,7 +105,48 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+       $product = Product::find($id);
+      
+        $product->name = $request->name;
+        if($request->slug == $product->slug){
+            $product->slug=false;
+        }
+        else{
+            $product->slug = Str::slug($request->get('name'));
+        }
+        
+        $product->price = $request->price;
+        $product->special_price = $request->special_price;
+        $product->special_price_start = $request->special_price_start;
+        $product->special_price_end = $request->special_price_end;
+        $product->selling_price = $request->selling_price;
+        $product->qty = $request->qty;
+        $product->new_from = $request->new_from;
+        $product->new_to = $request->new_to;
+        $product->description = $request->description;
+        $product->manage_stock = $request->manage_stock== 'on' ? '1' : '0';;
+        $product->in_stock = $request->in_stock== 'on' ? '1' : '0';;
+        $product->is_active = $request->is_active== 'on' ? '1' : '0';;
+       
+        $rand = str_random(4) . '.'.Str::slug($request->get('name'));
+
+        $product->sku = $rand;
+
+        //set path to http://localhost/ecommerce/public/Productimg/
+        if($request->hasfile('image')){
+            $dir =  public_path('Productimg/');
+        if ($product->image != '' && File::exists($dir . $product->image))File::delete($dir . $product->image);
+        
+        $extension = strtolower($request->file('image')->getClientOriginalExtension()); 
+        $fileName = str_random() . '.' . $extension; // rename image
+        $request->file('image')->move($dir, $fileName);//move img
+        $product->image = $fileName;//add to object
+        }// get image extension
+        
+        
+        
+        $product->save();
+        return redirect()->back() ->with('alert', 'Updated!');
     }
 
     /**
@@ -114,6 +157,8 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $new = Product::find($id);
+        $new->delete();
+       return Redirect()->route('product.index')->with('warning','Product Deleted successfully!');
     }
 }
