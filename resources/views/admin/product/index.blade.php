c2fc52cd7e8e971ec2e20ff21819ade86643b165
Kabir <khyrulkabir01@gmail.com>
2019-07-15 12:19:59 +0600

    Update index.blade.php

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -3,121 +3,61 @@
 <link href="{{asset('backend\plugins\datatable\dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 @stop
 @section('content')
-<div class="row">
-							<div class="col-md-12 col-lg-6">
+<div class=" content-area">
+						<div class="page-header">
+							<h4 class="page-title">Data Tables</h4>
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="#">Tables</a></li>
+								<li class="breadcrumb-item active" aria-current="page">Data Tables</li>
+							</ol>
+						</div>
+						<div class="row">
+							<div class="col-md-12 col-lg-12">
 								<div class="card">
 									<div class="card-header">
-										<h3 class="card-title">Full color variations</h3>
+										<div class="card-title">Data Tables</div>
 									</div>
-									<div class="table-responsive">
-										<table class="table card-table table-vcenter text-nowrap table-primary">
-											<thead class="bg-primary text-white">
-												<tr>
-													<th class="text-white">ID</th>
-													<th class="text-white">Name</th>
-													<th class="text-white">Position</th>
-													<th class="text-white">Salary</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<th scope="row">1</th>
-													<td>Joan Powell</td>
-													<td>Associate Developer</td>
-													<td>$450,870</td>
-												</tr>
-												<tr>
-													<th scope="row">2</th>
-													<td>Gavin Gibson</td>
-													<td>Account manager</td>
-													<td>$230,540</td>
-												</tr>
-												<tr>
-													<th scope="row">3</th>
-													<td>Julian Kerr</td>
-													<td>Senior Javascript Developer</td>
-													<td>$55,300</td>
-												</tr>
-												<tr>
-													<th scope="row">4</th>
-													<td>Cedric Kelly</td>
-													<td>Accountant</td>
-													<td>$234,100</td>
-												</tr>
-												<tr>
-													<th scope="row">5</th>
-													<td>Samantha May</td>
-													<td>Junior Technical Author</td>
-													<td>$43,198</td>
-												</tr>
-											</tbody>
-										</table>
+									<div class="card-body">
+										<div class="table-responsive">
+											<table id="example" class="table table-striped table-bordered border-top-0 border-bottom-0" style="width:100%">
+												<thead>
+													<tr class="border-bottom-0">
+														<th class="wd-15p">First name</th>
+														<th class="wd-15p">Last name</th>
+														<th class="wd-20p">Position</th>
+														<th class="wd-15p">Start date</th>
+														<th class="wd-10p">Salary</th>
+														<th class="wd-25p">E-mail</th>
+													</tr>
+												</thead>
+												<tbody>
+													<tr>
+														<td>Bella</td>
+														<td>Chloe</td>
+														<td>System Developer</td>
+														<td>2018/03/12</td>
+														<td>$654,765</td>
+														<td>b.Chloe@datatables.net</td>
+													</tr>
+													
+												</tbody>
+											</table>
+										</div>
 									</div>
-									<!-- table-responsive -->
+									<!-- table-wrapper -->
 								</div>
-							</div>
-
-							<div class="col-md-12 col-lg-6">
-								<div class="card">
-									<div class="card-header">
-										<h3 class="card-title">Full color variations2</h3>
-									</div>
-									<div class="table-responsive">
-										<table class="table card-table table-vcenter text-nowrap table-warning">
-											<thead class="bg-warning text-white">
-												<tr>
-													<th>ID</th>
-													<th>Name</th>
-													<th>Position</th>
-													<th>Salary</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<th scope="row">1</th>
-													<td>Joan Powell</td>
-													<td>Associate Developer</td>
-													<td>$450,870</td>
-												</tr>
-												<tr>
-													<th scope="row">2</th>
-													<td>Gavin Gibson</td>
-													<td>Account manager</td>
-													<td>$230,540</td>
-												</tr>
-												<tr>
-													<th scope="row">3</th>
-													<td>Julian Kerr</td>
-													<td>Senior Javascript Developer</td>
-													<td>$55,300</td>
-												</tr>
-												<tr>
-													<th scope="row">4</th>
-													<td>Cedric Kelly</td>
-													<td>Accountant</td>
-													<td>$234,100</td>
-												</tr>
-												<tr>
-													<th scope="row">5</th>
-													<td>Samantha May</td>
-													<td>Junior Technical Author</td>
-													<td>$43,198</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-									<!-- table-responsive -->
-								</div>
+								<!-- section-wrapper -->
 							</div>
 						</div>
+					</div>
 @stop
 @section('js')
 <!-- Data tables -->
-		<script src="{{asset('plugins\datatable\jquery.dataTables.min.js')}}"></script>
-		<script src="{{asset('plugins\datatable\dataTables.bootstrap4.min.js')}}"></script>
+		<script src="{{asset('backend/plugins\datatable\jquery.dataTables.min.js')}}"></script>
+		<script src="{{asset('backend/plugins\datatable\dataTables.bootstrap4.min.js')}}"></script>
 
 		<!-- Select2 js -->
-		<script src="{{asset('plugins\select2\select2.full.min.js')}}"></script>
+		<script src="{{asset('backend/plugins\select2\select2.full.min.js')}}"></script>
 <script>
 $(function(e) {
 $('#example').DataTable();
